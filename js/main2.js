96b8c16adc2fafe8fca3efdf8011d35f7bad4a8a
AngeloRoudani <ange.rouda@gmail.com>
2023-02-21 17:37:53 +0100

    concluso

---
+++ js/main2.js
@@ -0,0 +1,18 @@
+/*Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23*/
+document.getElementById("myBtn").onclick = myObject;
+
+    const firstName = prompt('insersci nome');
+
+    const userSurname = prompt('inserisci cognome');
+    
+    const userColor = prompt('inserisci colore preferito');
+    
+    const passNumber = '23';
+
+function myObject (firstName, userSurname, userColor) {
+    
+    document.getElementById('password').innerHTML = "La Password è:" + firstName + userSurname + userColor + passNumber;
+}
